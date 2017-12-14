74b40be532fbb258f6734a89eee24b9eed641fcf
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2017-12-14 15:41:55 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,6 +19,7 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   WIN_COMBINATIONS.detect do |win_combination|
+    puts win_combination
     win_index_1 = win_combination[0]
     win_index_2 = win_combination[1]
     win_index_3 = win_combination[2]
@@ -26,7 +27,9 @@ def won?(board)
     position_1 = board[win_index_1]
     position_2 = board[win_index_2]
     position_3 = board[win_index_3]
-
+puts position_1
+puts position_2
+puts position_3
     if (position_1 == "X" && position_2 == "X" && position_3 == "X")
       win_combination
     else
